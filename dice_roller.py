63afab09bc82dd8f9ebee73e5639b001c6f8b937
marcopav <marcopav@live.it>
2020-05-28 00:21:09 +0200

    Now rolling from 1 to 6 dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,16 @@
+from random import randint
+
+ordinals=[ 'first' , 'second' , 'third' , 'forth' , 'fifth' , 'sixth' ,]
 def main():
-  print('You rolled a die')
+  dice_rolls= int(input('how many rolls (max 6)?: '))
+  while dice_rolls<1 or dice_rolls>6 :
+    dice_rolls= int(input('Not valid. Retry: '))
+  dice_sum=0
+  for i in range(dice_rolls):
+    roll = randint(1,6)
+    print(f'As {ordinals[i]}, you rolled a {roll}')
+    dice_sum+=roll
+  print(f'You have rolled a total of {dice_sum}')
 
 if __name__== "__main__":
   main()
